939a7fc9c4c76c57a8a1dff65137587f0ebd5705
cuneyt <cuneytky@gmail.com>
2023-08-22 20:44:45 +0300

    part-1 added

---
+++ src/main/java/com/cydeo/service/CrudService.java
@@ -0,0 +1,10 @@
+package com.cydeo.service;
+
+import java.util.List;
+
+public interface CrudService<T,ID> {
+    T save(T role);
+    T findById(ID username);
+    List<T> findAll();
+    void deleteById(ID username);
+}
